7e2bb59f3e561b8b6250ee2c1e3c48a3b9ade72d
Nest2f <angelo_jmg_52@hotmail.com>
2018-01-23 22:44:50 -0500

    css completo y nueva vista

---
+++ Prototipo2Dapper/views/Register.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Entidades;
+using LogicaNegocios;
+
+namespace Prototipo2Dapper.views
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void register_Click(object sender, EventArgs e)
+        {
+            if (license.Checked == true)
+            {
+                UsuarioLN data = new UsuarioLN();
+                List<Usuario> lst = data.Mostrar();
+                Boolean noExiste = true;
+                Usuario item = new Usuario();
+                item.ID = 0;
+                item.nombre = register_nombres.Text;
+                item.apellidos = register_apellidos.Text;
+                //item.ci = register_cedula.Text;
+                //item.contrasenia = register_passowrd.Text;
+                //item.FirmaSello = "";
+                //DateTime dateTime = DateTime.Parse(register_fechaNacimiento.Text);
+                //item.fechaNacimiento = dateTime;
+                //Sexo sexo = new Sexo();
+                //sexo.id_sexo = Convert.ToInt32(DropDownListSexo.SelectedValue);
+                //item.sexo = sexo;
+                //FormacionProfecional formacion = new FormacionProfecional();
+                //formacion.id_formacionProfecional = Convert.ToInt32(DropDownListFormacionProfecional.SelectedValue);
+                //item.formacionProfecional = formacion;
+                //Especialidad especialidad = new Especialidad();
+                //especialidad.id_especialidad = Convert.ToInt32(DropDownListEspecialidad.SelectedValue);
+                //item.especialidad = especialidad;
+                //Nacionalidad nacionalidad = new Nacionalidad();
+                //nacionalidad.id_nacionalidad = Convert.ToInt32(DropDownListNacionalidad.SelectedValue);
+                //item.nacionalidad = nacionalidad;
+                //Autoidetificacion autoidetificacion = new Autoidetificacion();
+                //autoidetificacion.id_autoidetificacion = Convert.ToInt32(DropDownListAutoidentificación.SelectedValue);
+                //item.autoidetificacion = autoidetificacion;
+                //CodigoMPS codigoMPSLN = new CodigoMPS();
+                //codigoMPSLN.id_codigoMPS = Convert.ToInt32(DropDownListCodigoMPS.SelectedValue);
+                //item.codigoMPS = codigoMPSLN;
+                //item.foto = "graficos/avatar-2.jpg";
+                foreach (Usuario ite in lst)
+                {
+                    if (ite.ID.Equals(item.ID))
+                    {
+                        noExiste = false;
+                    }
+
+                }
+                if (noExiste)
+                {
+                    data.Insertar(item);//inserta y se va home 
+                    ScriptManager.RegisterStartupScript(this, GetType(), "Popup", "successCreateAcount();", true);
+                    //  Response.Redirect(GetRouteUrl("LoginRoute", null));
+                }
+                else
+                {
+                    ScriptManager.RegisterStartupScript(this, GetType(), "Popup", "errorAlertUsuarioExiste();", true);
+                }
+            }
+            else
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(), "Popup", "errorAlertTerminos();", true);
+            }
+
+        }
+    }
+}
---
+++ Prototipo2Dapper/views/Register.aspx.designer.cs
@@ -0,0 +1,195 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Prototipo2Dapper.views {
+    
+    
+    public partial class Register {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control register_cedula.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox register_cedula;
+        
+        /// <summary>
+        /// Control register_nombres.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox register_nombres;
+        
+        /// <summary>
+        /// Control register_apellidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox register_apellidos;
+        
+        /// <summary>
+        /// Control ObjectDataSourceSexo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ObjectDataSource ObjectDataSourceSexo;
+        
+        /// <summary>
+        /// Control DropDownListSexo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownListSexo;
+        
+        /// <summary>
+        /// Control ObjectDataSourceFormacionProfecional.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ObjectDataSource ObjectDataSourceFormacionProfecional;
+        
+        /// <summary>
+        /// Control DropDownListFormacionProfecional.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownListFormacionProfecional;
+        
+        /// <summary>
+        /// Control ObjectDataSourceEspecialidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ObjectDataSource ObjectDataSourceEspecialidad;
+        
+        /// <summary>
+        /// Control DropDownListEspecialidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownListEspecialidad;
+        
+        /// <summary>
+        /// Control ObjectDataSourceNacionalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ObjectDataSource ObjectDataSourceNacionalidad;
+        
+        /// <summary>
+        /// Control DropDownListNacionalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownListNacionalidad;
+        
+        /// <summary>
+        /// Control ObjectDataSourceAutoidentificación.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ObjectDataSource ObjectDataSourceAutoidentificación;
+        
+        /// <summary>
+        /// Control DropDownListAutoidentificación.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownListAutoidentificación;
+        
+        /// <summary>
+        /// Control ObjectDataSourceCodigoMPS.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ObjectDataSource ObjectDataSourceCodigoMPS;
+        
+        /// <summary>
+        /// Control DropDownListCodigoMPS.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownListCodigoMPS;
+        
+        /// <summary>
+        /// Control register_fechaNacimiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox register_fechaNacimiento;
+        
+        /// <summary>
+        /// Control register_passowrd.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox register_passowrd;
+        
+        /// <summary>
+        /// Control license.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox license;
+        
+        /// <summary>
+        /// Control register.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button register;
+    }
+}
