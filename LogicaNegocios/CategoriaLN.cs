1e80d712eb5e9d6355f68d1185a18fc3885b04e9
Nest2f <angelo_jmg_52@hotmail.com>
2018-02-01 03:01:08 -0500

    todas mis actualizaciones

---
+++ LogicaNegocios/CategoriaLN.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using AccesoDatos;
+
+namespace LogicaNegocios
+{
+    public class CategoriaLN
+    {
+        public void Insertar(Categoria categoria)
+        {
+            CategoriaAD acceso = new CategoriaAD();
+            acceso.InsertCategorie(categoria);
+        }
+        public void Update(Categoria categoria)
+        {
+            CategoriaAD acceso = new CategoriaAD();
+            acceso.UpdateCategorie(categoria);
+        }
+        public List<Categoria> Mostrar()
+        {
+            CategoriaAD acceso = new CategoriaAD();
+            return acceso.MostrarCategoria();
+        }
+        public Categoria CategoriaID(Categoria categoria)
+        {
+            CategoriaAD acceso = new CategoriaAD();
+            return acceso.CategoriaID(categoria);
+        }
+    }
+}
